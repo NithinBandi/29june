005ee843485446c24a1552c2573bf0547064353a
NithinBandi <nithinbandi4@gmail.com>
2024-07-27 12:14:40 +0530

    new one

---
+++ basic/src/constructors/employee.java
@@ -0,0 +1,11 @@
+package constructors;
+import java.util.Scanner;
+public class employee {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+
+	}
+
+}
--- basic/src/constructors/persondetls.java
+++ basic/src/constructors/persondetls.java
@@ -6,11 +6,25 @@ person with their name and age*/
 package constructors;
 import java.util.Scanner;
 public class persondetls {
-
-	persondetls{
+    private int age;
+    private String name;
+	public persondetls(String name,int age)
+	{
+		this.name=name;
+		this.age=age;
 		
 	}
+	public void introduce()
+	{
+		System.out.println("name"+name+"age"+age);
+	}
+	 
+	
 	public static void main(String[] args) {
+		persondetls person1=new persondetls("king",22);
+		person1.introduce();
+		persondetls person2=new persondetls("love",20);
+		person2.introduce();
 		
 
 	}

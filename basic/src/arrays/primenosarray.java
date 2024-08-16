41c9cb9e894f4da6a0783be43036a6e0e63f7a17
NithinBandi <nithinbandi4@gmail.com>
2024-08-16 09:19:15 +0530

    new

--- basic/src/arrays/primenosarray.java
+++ basic/src/arrays/primenosarray.java
@@ -7,13 +7,48 @@ public class primenosarray {
 	Scanner sc=new Scanner(System.in);
 	System.out.println("enter the how many numbers are to be entered");
 	int n=sc.nextInt();
-	int a[]=new int[];
+	int a[]=new int[n];
 	System.out.println("enter the elements");
 	for(int i=0;i<n;i++)
 	{
 		a[i]=sc.nextInt();
 	}
-
+	for(int i=0;i<n;i++) {
+    if(isprime(a[i]))
+    {
+    	System.out.println("it is a prime number:"+ a[i]);
+    }
+	}
+	System.out.println(" ");
+	}
+	public static boolean isprime(int num)
+	{
+		
+		if(num<=1)
+		{
+			return false;
+		}
+		if(num==2)
+		{
+			return true;
+		}
+		if(num%2==0)
+		{
+			return false;
+		}
+		
+		for(int i=3;i<num/2;i=i+2)
+		{
+			if(num%i==0)
+			{
+				return false;
+			}
+		}return true;
+		
+	
+	
+	
+	
 	}
 
 }

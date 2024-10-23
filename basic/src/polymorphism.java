a5ac62904b911a30b5c3b5c31d5d20d76a9aa6e2
NithinBandi <nithinbandi4@gmail.com>
2024-10-23 14:24:21 +0530

    new

--- basic/src/polymorphism.java
+++ basic/src/polymorphism.java
@@ -1,9 +1,35 @@
-
+class animal{
+	public void makesound() {
+	System.out.println("animal makes sound");
+}}
+class dog extends animal {
+	public void makesound() {
+		System.out.println("dog barks");
+	}
+}
+class cat extends animal {
+	public void makesound() {
+	System.out.println("cat meows");
+}
+}
+class calculator {
+	public int add(int a,int b) {
+		return a+b;
+	}
+	public double add(double a,double b) {
+		return a+b;
+	}
+}
 public class polymorphism {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		dog jermanshepard=new dog();
+		jermanshepard.makesound();
+		cat james=new cat();
+		james.makesound();
+		calculator cal=new calculator();
+		System.out.println("addition of two numbers is:"+cal.add(5,13));
+		System.out.println("addition of two numbers is:"+cal.add(5.5,13.6));
 	}
 
 }
